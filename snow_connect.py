2a664b66d77cc70ca2df4d15e42c32800b4147c5
hrushikeshth <hrushikeshthorat@outlook.com>
2024-05-17 13:01:59 +0530

    Update 4

--- snow_connect.py
+++ snow_connect.py
@@ -3,6 +3,5 @@ import pandas as pd
 
 def get_tables2():
         conn = st.connection("snowflake")
-        query = "SELECT TABLE_NAME FROM AMZ_VENDOR_DATA.INFORMATION_SCHEMA.TABLES"
-        df = pd.read_sql(query, conn)
+        df = conn.query("SELECT TABLE_NAME FROM AMZ_VENDOR_DATA.INFORMATION_SCHEMA.TABLES;", ttl=600)
         return df["TABLE_NAME"].tolist()
