6ad4bf9ecf92a6d848932a7ab90c642eee582c34
hrushikeshth <hrushikeshthorat@outlook.com>
2024-05-17 16:31:28 +0530

    Slight update

--- snow-arctic-gpt.py
+++ snow-arctic-gpt.py
@@ -86,9 +86,6 @@ st.sidebar.button('Clear chat history', on_click=clear_chat_history)
 st.sidebar.caption('App by [Hrushi](https://www.linkedin.com/in/hrushikeshth/) as an Entrant in [The Future of AI is Open (Hackathon)](https://arctic-streamlit-hackathon.devpost.com/), demonstrating the new LLM by Snowflake called [Snowflake Arctic](https://www.snowflake.com/blog/arctic-open-and-efficient-foundation-language-models-snowflake)')
 # st.sidebar.caption('The app repository can be found [here](https://github.com/hrushikeshth/slit-arctic)')
 
-# Display the tables in a dropdown menu
-selected_table = st.selectbox("Select a table", tables)
-
 # To make sure user aren't sending too much text to the Model
 @st.cache_resource(show_spinner=False)
 def get_tokenizer():
